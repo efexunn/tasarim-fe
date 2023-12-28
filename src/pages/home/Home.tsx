88e1d783618d36552e16593336dab1f19c81d3bc
Efe Bozkurt <bozkurtefe64@gmail.com>
2023-12-28 20:11:34 +0300

    filters added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { LecturerService } from "../../services/LecturerService";
 import { DoctorService } from "../../services/DoctorServices";
-import { Card, Modal } from "antd";
+import { Card, Modal, Select } from "antd";
 import "./home.scss";
 import PersonDetail from "../../components/person-detail/PersonDetail";
 
@@ -10,19 +10,19 @@ const Home = () => {
   let doctorService = new DoctorService();
   let emptyDoc = {
     DoctorName: "",
-        DoctorEmail: "String",
-        DoctorTitle: 1,
-        DoctorTitleName: "String",
-        DoctorHospital: 1,
-        DoctorHospitalName: "string",
-        DoctorPoliclinic: 1,
-        DoctorPoliclinicName: "string",
-        Id: 1,
-        RowStateId: 1
-  }
-  let [doctor, setDoctor] = useState<DoctorModel>(emptyDoc)
+    DoctorEmail: "String",
+    DoctorTitle: 1,
+    DoctorTitleName: "String",
+    DoctorHospital: 1,
+    DoctorHospitalName: "string",
+    DoctorPoliclinic: 1,
+    DoctorPoliclinicName: "string",
+    Id: 1,
+    RowStateId: 1,
+  };
+  let [doctor, setDoctor] = useState<DoctorModel>(emptyDoc);
   const [open, setOpen] = useState(false);
-  const showModal = (item : DoctorModel) => {
+  const showModal = (item: DoctorModel) => {
     setDoctor(item);
     setOpen(true);
   };
@@ -33,177 +33,97 @@ const Home = () => {
     setOpen(false);
   };
 
-  let orn = [
+  useEffect(() => {
+    doctorService.getDoctors().then((response) => {
+      setDoctorList(response.data);
+    });
+    //.then((response) => setLecturerList(response.data));
+  }, []);
+
+  let opts = [
     {
-      FullName: "prof. dr. kemal fidanboylu",
-      Title: "",
-      Email: " kfidan@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 295 5282",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 3.",
-      AvesisLink: " https://avesis.uludag.edu.tr/kfidan",
-      Id: 150,
-      RowStateId: 0,
+      value: "1",
+      label: "Not Identified",
     },
     {
-      FullName: "prof. dr. kemal fidanboylu",
-      Title: "",
-      Email: " kfidan@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 295 5282",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 3.",
-      AvesisLink: " https://avesis.uludag.edu.tr/kfidan",
-      Id: 151,
-      RowStateId: 0,
+      value: "2",
+      label: "Closed",
     },
     {
-      FullName: "prof. dr. ahmet emir dirik",
-      Title: "",
-      Email: " edirik@uludag.edu.tr",
-      TelephoneNumber: " +90(224) 294 0655",
-      Adress: " bilgisayar müh. bölüm binası, 5. kat, oda 546.",
-      AvesisLink: " https://avesis.uludag.edu.tr/edirik",
-      Id: 152,
-      RowStateId: 0,
+      value: "3",
+      label: "Communicated",
     },
     {
-      FullName: "prof. dr. ahmet emir dirik",
-      Title: "",
-      Email: " edirik@uludag.edu.tr",
-      TelephoneNumber: " +90(224) 294 0655",
-      Adress: " bilgisayar müh. bölüm binası, 5. kat, oda 546.",
-      AvesisLink: " https://avesis.uludag.edu.tr/edirik",
-      Id: 153,
-      RowStateId: 0,
+      value: "4",
+      label: "Identified",
     },
     {
-      FullName: "prof. dr. pınar kırcı",
-      Title: "",
-      Email: " pinarkirci@uludag.edu.tr",
-      TelephoneNumber: "",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 110.",
-      AvesisLink: " https://avesis.uludag.edu.tr/pinarkirci",
-      Id: 154,
-      RowStateId: 0,
+      value: "5",
+      label: "Resolved",
     },
     {
-      FullName: "prof. dr. pınar kırcı",
-      Title: "",
-      Email: " pinarkirci@uludag.edu.tr",
-      TelephoneNumber: "",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 110.",
-      AvesisLink: " https://avesis.uludag.edu.tr/pinarkirci",
-      Id: 155,
-      RowStateId: 0,
-    },
-    {
-      FullName: "doç. dr. gıyasettin özcan",
-      Title: "",
-      Email: " gozcan@uludag.edu.tr",
-      TelephoneNumber: "+90 (224) 294 2792",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 107.",
-      AvesisLink: " https://avesis.uludag.edu.tr/gozcan",
-      Id: 156,
-      RowStateId: 0,
-    },
-    {
-      FullName: "doç. dr. gıyasettin özcan",
-      Title: "",
-      Email: " gozcan@uludag.edu.tr",
-      TelephoneNumber: "+90 (224) 294 2792",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 107.",
-      AvesisLink: " https://avesis.uludag.edu.tr/gozcan",
-      Id: 157,
-      RowStateId: 0,
-    },
-    {
-      FullName: "doç. dr. metin bilgin",
-      Title: "",
-      Email: " metinbilgin@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 275 5263",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda:109",
-      AvesisLink: " https://avesis.uludag.edu.tr/metinbilgin",
-      Id: 158,
-      RowStateId: 0,
-    },
-    {
-      FullName: "doç. dr. metin bilgin",
-      Title: "",
-      Email: " metinbilgin@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 275 5263",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda:109",
-      AvesisLink: " https://avesis.uludag.edu.tr/metinbilgin",
-      Id: 159,
-      RowStateId: 0,
-    },
-    {
-      FullName: "doç. dr. murtaza cicioğlu",
-      Title: "",
-      Email: " murtazacicioglu@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 294 2796",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 111.",
-      AvesisLink: " https://avesis.uludag.edu.tr/murtazacicioglu",
-      Id: 160,
-      RowStateId: 0,
-    },
-    {
-      FullName: "doç. dr. murtaza cicioğlu",
-      Title: "",
-      Email: " murtazacicioglu@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 294 2796",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 111.",
-      AvesisLink: " https://avesis.uludag.edu.tr/murtazacicioglu",
-      Id: 161,
-      RowStateId: 0,
-    },
-    {
-      FullName: "dr. öğr. üyesi ceyda nur öztürk",
-      Title: "",
-      Email: " ceydanur@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 294 1936",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 112",
-      AvesisLink: " https://avesis.uludag.edu.tr/ceydanur",
-      Id: 162,
-      RowStateId: 0,
-    },
-    {
-      FullName: "dr. öğr. üyesi ceyda nur öztürk",
-      Title: "",
-      Email: " ceydanur@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 294 1936",
-      Adress: " bilgisayar müh. bölüm binası, 1. kat, oda 112",
-      AvesisLink: " https://avesis.uludag.edu.tr/ceydanur",
-      Id: 163,
-      RowStateId: 0,
-    },
-    {
-      FullName: "araş. gör. caner şen",
-      Title: "",
-      Email: " canersen@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 294 2644",
-      Adress: " bilgisayar müh. bölüm binası, 1.kat, oda 105.",
-      AvesisLink: " https://avesis.uludag.edu.tr/canersen",
-      Id: 164,
-      RowStateId: 0,
-    },
-    {
-      FullName: "araş. gör. caner şen",
-      Title: "",
-      Email: " canersen@uludag.edu.tr",
-      TelephoneNumber: " +90 (224) 294 2644",
-      Adress: " bilgisayar müh. bölüm binası, 1.kat, oda 105.",
-      AvesisLink: " https://avesis.uludag.edu.tr/canersen",
-      Id: 165,
-      RowStateId: 0,
+      value: "6",
+      label: "Cancelled",
     },
   ];
 
-  useEffect(() => {
-    doctorService.getDoctors().then((response) => {setDoctorList(response.data)})
-      //.then((response) => setLecturerList(response.data));
-  }, []);
-
+  let [cityCategory, setCityCategory] = useState("Hastane");
+  let [titleCategory, setTitleCategory] = useState("Ünvan");
+  let [policlinicCategory, setPoliclinicCategory] = useState("Poliklinik");
   return (
     <>
       <div className="home">
+        <div className="filters">
+          <Select
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Search to Select"
+            optionFilterProp="children"
+            filterOption={(input, option) =>
+              (option?.label ?? "").includes(input)
+            }
+            options={opts}
+            value={cityCategory}
+            onChange={(cityCategory) => {
+              setCityCategory(cityCategory);
+              console.log(cityCategory);
+            }}
+          />
+
+          <Select
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Search to Select"
+            optionFilterProp="children"
+            filterOption={(input, option) =>
+              (option?.label ?? "").includes(input)
+            }
+            options={opts}
+            value={titleCategory}
+            onChange={(titleCategory) => {
+              setTitleCategory(titleCategory);
+              console.log(titleCategory);
+            }}
+          />
+
+          <Select
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Search to Select"
+            optionFilterProp="children"
+            filterOption={(input, option) =>
+              (option?.label ?? "").includes(input)
+            }
+            options={opts}
+            value={policlinicCategory}
+            onChange={(policlinicCategory) => {
+              setPoliclinicCategory(policlinicCategory);
+              console.log(policlinicCategory);
+            }}
+          />
+          <button>Filtrele</button>
+        </div>
+
         <Card
           style={{
             display: "flex",
@@ -212,23 +132,21 @@ const Home = () => {
           }}
         >
           <Modal
-          open={open}
-          onOk={handleOk}
-          onCancel={handleCancel}
-          width={800}
-          okButtonProps={{
-           
-          }}
-          cancelButtonProps={{
-            disabled: true,
-          }}
+            open={open}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            width={800}
+            okButtonProps={{}}
+            cancelButtonProps={{
+              disabled: true,
+            }}
           >
-            <PersonDetail doctorObj={doctor}/>
+            <PersonDetail doctorObj={doctor} />
           </Modal>
           {doctorList.map((item) => (
             <Card.Grid
               style={{ width: "17%", marginInline: "4%", height: "auto" }}
-              onClick={()=>showModal(item)}
+              onClick={() => showModal(item)}
             >
               <div className="card">
                 <div className="photo">
